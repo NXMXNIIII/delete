64697ea03b1f7ce968103251b0acad9de5d36ea9
NXMXNIIII <gabrielmariona.pro@gmail.com>
2021-11-20 21:10:28 +0100

    querySelectorAll()

--- script.js
+++ script.js
@@ -2,29 +2,19 @@ function getStyleID(idTarget,stylePropriete){
     return document.defaultView.getComputedStyle(document.getElementById(idTarget)).getPropertyValue(stylePropriete);
 }
 
-document.getElementById("div1").addEventListener("mouseover",function(){
-    var initialColor = getStyleID("div1","background-color");
-    if (initialColor == "rgb(67, 65, 165)")
-        this.style.backgroundColor = "rgb(201, 79, 79)";
-    else
-        this.style.backgroundColor = "rgb(67, 65, 165)";
-})
+var myIds = document.querySelectorAll('.box')
 
-document.getElementById("div2").addEventListener("mouseover",function(){
-    var initialColor = getStyleID("div2","background-color");
-    if (initialColor == "rgb(67, 65, 165)")
-        this.style.backgroundColor = "rgb(4, 247, 247)";
-    else
-        this.style.backgroundColor = "rgb(67, 65, 165)";
-})
-
-// ** Si j'inverse if et else, le cube a une latence de 1 click **
-document.getElementById("div3").addEventListener("mouseover",function(){
-    if(this.style.backgroundColor=='yellow'){
-        this.style.backgroundColor='brown';
-    }
-    else{
-        this.style.backgroundColor='yellow';
-    }
-
-})
+for(var i = 0; i < myIds.length;i++){
+    myIds[i].style.transition= "all 1s";
+    myIds[i].addEventListener('mouseover',function(){
+        if (this.style.backgroundColor == "rgb(4, 247, 247)"){
+            this.style.backgroundColor = "rgb(42, 165, 138)"
+        }
+        else if(this.style.backgroundColor == "rgb(42, 165, 138)"){
+            this.style.backgroundColor = "rgb(4, 247, 247)"
+        }
+        else{
+            this.style.backgroundColor = "rgb(4, 247, 247)"
+        }
+    })
+}
